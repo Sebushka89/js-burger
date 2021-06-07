2d191149313fc93d244cf4014bfec25e03ea5dd2
Sebushka89 <sebastian.rizzo@hotmail.it>
2021-06-07 17:29:39 +0200

    ok

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 var createSum = document.getElementById("somma-button");
 
-var resultSum = document.getElementById("result-sum")
+var resultSum = document.getElementById("price")
 
 
 createSum.addEventListener("click", function() {
-    var resultSumInt = 0;
+    var resultSumInt = 50;
     var checkNum = document.getElementsByClassName("check-ingredients");
     for (var i = 0; i < checkNum.length; i++) {
         if (checkNum[i].checked) {
             resultSumInt += parseInt(checkNum[i].value);
         }
     }
-    resultSum.innerHTML = "$" + ' ' +  resultSumInt + resultSum ;
+    resultSum.innerHTML = "$" + ' ' +  resultSumInt  ;
 })
